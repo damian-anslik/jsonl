328e2c9ed7782dfa87378c1162acb44d04b250ff
Damian <danslik@interactivebrokers.ie>
2023-04-03 21:08:35 +0100

    add more examples and update docs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # About
 
 A simple Python library for working with JSONL (JSON Lines) files. For information on JSONL, please see: https://jsonlines.org/examples/
+
+Check out the `examples.py` file for ways to work with JSONL data. 
--- examples.py
+++ examples.py
@@ -1,15 +1,28 @@
 import jsonl
 
+
+def stream_json_lines_from_file(file_path: str) -> None:
+    for line in jsonl.load(file_path):
+        print(line)
+
+
 def load_json_lines_from_file(file_path: str) -> list[dict]:
-    lines = jsonl.load(file_path)
+    lines = [line for line in jsonl.load(file_path)]
     return lines
 
+
 def dump_json_lines_to_file(file_path: str, lines: list[dict]) -> None:
-    jsonl.dump(file_path, lines)
+    jsonl.dump(file_path, lines, file_mode="w")
+
+
+def append_json_lines_to_file(file_path: str, lines: list[dict]) -> None:
+    jsonl.dump(file_path, lines, file_mode="a")
+
 
 def load_json_lines_from_string(string: str) -> list[dict]:
     lines = jsonl.loads(string)
     return lines
+
 
 def dump_json_lines_to_string(lines: list[dict]) -> str:
     string = jsonl.dumps(lines)
