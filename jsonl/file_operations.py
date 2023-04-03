5f4ccf2bf62ee4fe2b907223881f2404eff37993
Damian <danslik@interactivebrokers.ie>
2023-04-03 21:08:21 +0100

    change loading from file behavior to streaming

--- jsonl/file_operations.py
+++ jsonl/file_operations.py
@@ -1,21 +1,26 @@
 import os
 import json
+import typing
 from jsonl import constants
 
 
-def load(file_path: os.PathLike) -> list[dict]:
+def load(file_path: os.PathLike) -> typing.Generator[dict, None, None]:
+    """  
+    Load a JSONL file and return a generator of objects. Each object is a line.
     """
-    Load a JSONL file into a list of objects, and return a list of objects.
-    """
-    with open(file_path, 'r', encoding=constants.FILE_ENCODING) as f:
-        lines = f.readlines()
-    return [json.loads(line) for line in lines]
+    FILE_MODE = 'r'
+    with open(file_path, FILE_MODE, encoding=constants.FILE_ENCODING) as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            yield json.loads(line)
 
 
-def dump(file_path: os.PathLike, lines: list[dict]) -> None:
+def dump(file_path: os.PathLike, lines: list[dict], file_mode: str = "w") -> None:
     """
     Dump a list of objects into a JSONL file. Each object is a line.
     """
-    with open(file_path, 'w', encoding=constants.FILE_ENCODING) as f:
+    with open(file_path, file_mode, encoding=constants.FILE_ENCODING) as f:
         for line in lines:
             f.write(json.dumps(line) + constants.LINE_SEPARATOR)
